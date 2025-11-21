484ee8dc1a2942ae5ef52b11d257d4374ce15cee
mili-emzz <m37009146@gmail.com>
2025-11-21 15:03:56 -0600

    login ang bottom nav changes

--- app/src/main/java/com/emiliagomez/vanamiapp/MainActivity.kt
+++ app/src/main/java/com/emiliagomez/vanamiapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.emiliagomez.vanamiapp.presentation.LoginView
 import com.emiliagomez.vanamiapp.ui.theme.VanamiAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,24 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VanamiAppTheme {
-
+                LoginView()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VanamiAppTheme {
-        Greeting("Android")
     }
 }
