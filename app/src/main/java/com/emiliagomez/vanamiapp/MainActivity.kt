f0f645aac635d11950e0ecb138e47447446f0657
naddiluv <nadiaguerrc@gmail.com>
2025-11-23 14:00:45 -0600

    estilos en vista de calendario

--- app/src/main/java/com/emiliagomez/vanamiapp/MainActivity.kt
+++ app/src/main/java/com/emiliagomez/vanamiapp/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.emiliagomez.vanamiapp.presentation.views.CalendarView
 import com.emiliagomez.vanamiapp.ui.theme.VanamiAppTheme
 
@@ -20,25 +13,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VanamiAppTheme {
-                CalendarView()
-
+                CalendarView(
+                    onDayClick = { selectedDate ->
+                        // Lógica al pulsar día
+                    },
+                    imageResId = R.drawable.ic_launcher_foreground, // Cambia por tu imagen real
+                    onDiaryClick = {
+                        // Lógica al pulsar “Diario”
+                    }
+                )
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VanamiAppTheme {
-        Greeting("Android")
     }
 }
