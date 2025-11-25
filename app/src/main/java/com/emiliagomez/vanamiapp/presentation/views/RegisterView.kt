952b140a7ddb73221a64feedf4ff1d90bd759c9d
mili-emzz <m37009146@gmail.com>
2025-11-24 21:55:54 -0600

    registerview file changed

--- app/src/main/java/com/emiliagomez/vanamiapp/presentation/views/RegisterView.kt
+++ app/src/main/java/com/emiliagomez/vanamiapp/presentation/views/RegisterView.kt
@@ -1,15 +1,11 @@
-package com.emiliagomez.vanamiapp.presentation
+package com.emiliagomez.vanamiapp.presentation.views
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -18,9 +14,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
